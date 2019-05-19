af5f2d2e8a2059b71a5652473e92976d36743a27
abdulazees15 <50805591+abdulazees15@users.noreply.github.com>
2019-05-20 00:30:17 +0530

    Update pgm1.py

--- pgm1.py
+++ pgm1.py
@@ -1,10 +1,10 @@
-x = 'input()'
-if (x.isalpha()) == True:
-    print("invalid input")
-else:
+x = input()
+if (x.isnumeric()) == True:
     if x > 0:
         print("Positive")
     elif x < 0:
         print("Negative")
     else:
         print("zero")
+else:
+    print("invalid input")
