d94efa6e92b1c71b2e009da9f0716a659fb2ff84
abdulazees15 <50805591+abdulazees15@users.noreply.github.com>
2019-05-20 21:54:55 +0530

    Rename pgm2.c to pgm12.c

---
+++ pgm12.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int i,n,n1,rem,rev=0;
+scanf("%d",&n);
+n1=n;
+if(n1<=1000)
+{
+for(i=0;n>0;i++)
+{
+rem=n%10;
+rev=rev*10+rem;
+n=n/10;
+}
+if(n1==rev)
+printf("yes");
+else
+printf("no");
+}
+else
+printf("invalid");
+}
