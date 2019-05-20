10c315d4fc2a6d2189b569d042428bb1d5d0b83f
abdulazees15 <50805591+abdulazees15@users.noreply.github.com>
2019-05-20 22:52:37 +0530

    Create pgm16.c

---
+++ pgm16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main ()
+{
+   int a,b,i,j,count;
+   scanf("%d %d", &a, &b);
+   for (i =a+1; i<b;++i)
+   {
+      count= 0;
+      for (j=2;j<=i/2;++j)
+      {
+         if (i%j==0)
+         {
+            count=1;
+            break;
+         }
+      }
+      if (count==0) 
+          printf("%d ", i);
+   }
+   return 0;
+}
